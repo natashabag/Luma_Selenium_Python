bdad668f2a641e1716181fd3b30fcc42366f7c56
Natasha Bagramian <natalia.bagramian@gmail.com>
2024-04-20 20:50:20 -0700

    finished check_out

--- page_objects/base_page.py
+++ page_objects/base_page.py
@@ -5,7 +5,7 @@ from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
-
+#comment
 
 class BasePage:
     def __init__(self, driver: WebDriver):
