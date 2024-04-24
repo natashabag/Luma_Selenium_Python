a32110e192622e322a5d94fe60d87152ecb3aefc
Natasha Bagramian <natalia.bagramian@gmail.com>
2024-04-24 10:00:26 -0700

    corrected urls

--- page_objects/account_page.py
+++ page_objects/account_page.py
@@ -1,10 +1,22 @@
+from selenium.webdriver.remote.webdriver import WebDriver
+
 from page_objects.base_page import BasePage
 
 
 class AccountPage(BasePage):
-    __url = "https://magento.softwaretestingboard.com/customer/account/"
+    _url = "https://magento.softwaretestingboard.com/customer/account/"
 
+    # valid credentials:
+    __username = "annsmithtest@testmail.com"
+    __password = "MyPassword123!"
+
+    def __init__(self, driver: WebDriver):
+        self._driver = driver
+
+    @property
     def current_url(self) -> str:
-        return self.__url
+        return self._driver.current_url
 
-
+    @property
+    def expected_url(self) -> str:
+        return self._url
--- tests/test_account_page.py
+++ tests/test_account_page.py
@@ -10,5 +10,4 @@ class TestAccountPage:
         create_account_page = CreateAccountPage(driver)
         create_account_page.create_account()
         customer_page = AccountPage(driver)
-        expected_url = "https://magento.softwaretestingboard.com/customer/account/"
-        assert expected_url == customer_page.current_url(), "Actual URL is not the same as expected"
+        assert customer_page.expected_url == customer_page.current_url, "Actual URL is not the same as expected"
